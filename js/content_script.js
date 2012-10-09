1beaa621a740027c8acd0336fea3240960528d83
Jason Nadro <jason.nadro@gmail.com>
2012-10-08 23:24:26 -0500

    get local storage persistance of wikipedia breadcrumbs.  ability to clear the list.

--- js/content_script.js
+++ js/content_script.js
@@ -1,12 +1,28 @@
 $(document).ready(function () {
-	var myvar = localStorage.var;
-	if (myvar) {
+	var breadcrumbs = [];
+
+	var wikipediaTitle = document.title.split("-")[0];
+
+	if (localStorage['breadcrumbs']) {
+		breadcrumbs = JSON.parse(localStorage['breadcrumbs']);
+	}
+	else {
+		breadcrumbs.push(wikipediaTitle);
+	}
+	
+	if (breadcrumbs) {
 		$("body").append('<div id="research_trail_popup"></div>');
 		$("#research_trail_popup").html('<ul class="breadcrumb" id="research_trail"></ul>');
-		$("#research_trail").append('<li><a href="#">' + myvar + '</a> <span class="divider">/</span></li>');
-		$("#research_trail").append('<li><a href="#">Library</a> <span class="divider">/</span></li>');
-		$("#research_trail").append('<li class="active">Data</li>');
+
+		breadcrumbs.forEach(function(element, index) {
+			$("#research_trail").append('<li><a href="#">' + element + '</a> <span class="divider">/</span></li>');
+		});
+		$("#research_trail_popup").append('<button class="btn btn-mini" id="clear" type="button">Clear</button>');
+		$("#clear").click(function() {
+			localStorage.clear();
+		});
 	}
 
-	localStorage["var"] = "fuck yeah!";
+	breadcrumbs.push(wikipediaTitle);
+	localStorage["breadcrumbs"] = JSON.stringify(breadcrumbs);
 });
